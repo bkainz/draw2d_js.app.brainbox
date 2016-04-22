c62fd9d12e504d4472ac3c04b2ccc3991582d676
Herz, Andreas <andreas.herz@sap.com>
2016-04-22 17:07:01 +0200

    Updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -162,7 +162,7 @@ var Application = Class.extend(
 
 
     loginFirstMessage:function(){
-        $.bootstrapGrowl("You must first login into GITHUB to open a file from there!", {
+        $.bootstrapGrowl("You must first login into GITHUB to use this functionality", {
             type: 'danger',
             align: 'center',
             width: 'auto',
@@ -226,6 +226,32 @@ conf.fileSuffix = ".shape";
 conf.repository="http://freegroup.github.io/draw2d_js.shapes/assets/shapes/index.js";
 
 ;
+var EditEditPolicy = draw2d.policy.canvas.BoundingboxSelectionPolicy.extend({
+
+
+    init:function()
+    {
+      this._super();
+    },
+
+    /**
+     * @method
+     * Called by the canvas if the user click on a figure.
+     *
+     * @param {draw2d.Figure} the figure under the click event. Can be null
+     * @param {Number} mouseX the x coordinate of the mouse during the click event
+     * @param {Number} mouseY the y coordinate of the mouse during the click event
+     * @param {Boolean} shiftKey true if the shift key has been pressed during this event
+     * @param {Boolean} ctrlKey true if the ctrl key has been pressed during the event
+     *
+     * @since 3.0.0
+     */
+    onClick: function(figure, mouseX, mouseY, shiftKey, ctrlKey)
+    {
+        // do nothing in edit
+    }
+});
+;
 /*jshint sub:true*/
 
 
@@ -261,7 +287,10 @@ var Palette = Class.extend(
             $("#paletteElements").html(html);
 
             // Advanced filtering
-            $('#filter').on('keyup change', function () {
+            $('#filter').on('keyup change', function (event) {
+                if(event.keyCode===27){
+                    $('#filter').val("");
+                }
                 var val = this.value.toLowerCase();
                 $grid.shuffle('shuffle', function ($el, shuffle) {
                     var text = $.trim($el.data("name")).toLowerCase();
@@ -302,7 +331,37 @@ var Palette = Class.extend(
 });
 
 ;
+var SimulationEditPolicy = draw2d.policy.canvas.ReadOnlySelectionPolicy.extend({
+
+
+    init:function()
+    {
+      this._super();
+    },
+
+    /**
+     * @method
+     * Called by the canvas if the user click on a figure.
+     *
+     * @param {draw2d.Figure} the figure under the click event. Can be null
+     * @param {Number} mouseX the x coordinate of the mouse during the click event
+     * @param {Number} mouseY the y coordinate of the mouse during the click event
+     * @param {Boolean} shiftKey true if the shift key has been pressed during this event
+     * @param {Boolean} ctrlKey true if the ctrl key has been pressed during the event
+     *
+     * @since 3.0.0
+     */
+    onClick: function(figure, mouseX, mouseY, shiftKey, ctrlKey)
+    {
+        if(figure!==null){
+            figure.fireEvent("click", {x:mouseX, y:mouseY, shiftKey:shiftKey, ctrlKey:ctrlKey});
+            figure.onClick();
+        }
+    }
+});
+;
 /*jshint sub:true*/
+/*jshint evil:true */
 
 
 /**
@@ -319,22 +378,24 @@ var View = draw2d.Canvas.extend({
     {
         var _this = this;
 
-        this._super(id, 2000,2000);
-        this.grid =  new draw2d.policy.canvas.ShowGridEditPolicy(20);
+        this._super(id, 6000,6000);
+        this.simulate = false;
+        this.animationFrameFunc = $.proxy(this._calculate,this);
 
 
-        this.installEditPolicy( this.grid);
-
+        // register this class as event listener for the canvas
+        // CommandStack. This is required to update the state of
+        // the Undo/Redo Buttons.
+        //
+        this.getCommandStack().addEventListener(this);
 
         var router = new draw2d.layout.connection.InteractiveManhattanConnectionRouter();
         router.abortRoutingOnFirstVertexNode=false;
         var createConnection=function(sourcePort, targetPort){
             var c = new draw2d.Connection({
-                outlineColor:"#ffffff",
-                outlineStroke:1,
                 color:"#000000",
                 router: router,
-                stroke:1,
+                stroke:2,
                 radius:2
             });
             if(sourcePort) {
@@ -348,7 +409,7 @@ var View = draw2d.Canvas.extend({
         // install a Connection create policy which matches to a "circuit like"
         // connections
         //
-        this.installEditPolicy( new draw2d.policy.connection.ComposedConnectionCreatePolicy(
+        this.connectionPolicy = new draw2d.policy.connection.ComposedConnectionCreatePolicy(
                 [
                     // create a connection via Drag&Drop of ports
                     //
@@ -360,8 +421,76 @@ var View = draw2d.Canvas.extend({
                     new draw2d.policy.connection.OrthogonalConnectionCreatePolicy({
                         createConnection:createConnection
                     })
-                ])
-        );
+                ]);
+        this.installEditPolicy(this.connectionPolicy);
+
+        // show the ports of the elements only if the mouse cursor is close to the shape.
+        //
+        this.coronaFeedback = new draw2d.policy.canvas.CoronaDecorationPolicy();
+        this.installEditPolicy(this.coronaFeedback);
+
+        // nice grid decoration for the canvas paint area
+        //
+        this.grid =  new draw2d.policy.canvas.ShowGridEditPolicy(20);
+        this.installEditPolicy( this.grid);
+
+        // add some SnapTo policy for better shape/figure alignment
+        //
+        this.installEditPolicy( new draw2d.policy.canvas.SnapToGeometryEditPolicy());
+        this.installEditPolicy( new draw2d.policy.canvas.SnapToCenterEditPolicy());
+        this.installEditPolicy( new draw2d.policy.canvas.SnapToInBetweenEditPolicy());
+
+
+        this.installEditPolicy(new EditEditPolicy());
+
+        // Enable Copy&Past for figures
+        //
+        Mousetrap.bind(['ctrl+c', 'command+c'], $.proxy(function (event) {
+            var primarySelection = this.getSelection().getPrimary();
+            if(primarySelection!==null){
+                this.clippboardFigure = primarySelection.clone({excludePorts:true});
+                this.clippboardFigure.translate(5,5);
+            }
+            return false;
+        },this));
+        Mousetrap.bind(['ctrl+v', 'command+v'], $.proxy(function (event) {
+            if(this.clippboardFigure!==null){
+                var cloneToAdd = this.clippboardFigure.clone({excludePorts:true});
+                var command = new draw2d.command.CommandAdd(this, cloneToAdd, cloneToAdd.getPosition());
+                this.getCommandStack().execute(command);
+                this.setCurrentSelection(cloneToAdd);
+            }
+            return false;
+        },this));
+
+
+        // add keyboard support for shape/figure movement
+        //
+        Mousetrap.bind(['left'],function (event) {
+            var diff = _this.getZoom()<0.5?0.5:1;
+            var primarySelection = _this.getSelection().getPrimary();
+            if(primarySelection!==null){ primarySelection.translate(-diff,0);}
+            return false;
+        });
+        Mousetrap.bind(['up'],function (event) {
+            var diff = _this.getZoom()<0.5?0.5:1;
+            var primarySelection = _this.getSelection().getPrimary();
+            if(primarySelection!==null){ primarySelection.translate(0,-diff);}
+            return false;
+        });
+        Mousetrap.bind(['right'],function (event) {
+            var diff = _this.getZoom()<0.5?0.5:1;
+            var primarySelection = _this.getSelection().getPrimary();
+            if(primarySelection!==null){ primarySelection.translate(diff,0);}
+            return false;
+        });
+        Mousetrap.bind(['down'],function (event) {
+            var diff = _this.getZoom()<0.5?0.5:1;
+            var primarySelection = _this.getSelection().getPrimary();
+            if(primarySelection!==null){ primarySelection.translate(0,diff);}
+            return false;
+        });
+
 
 
         $("#editUndo").on("click", function(){
@@ -371,6 +500,21 @@ var View = draw2d.Canvas.extend({
         $("#editRedo").on("click", function(){
             _this.getCommandStack().redo();
         });
+
+
+        $("#simulationStart").on("click", function(){
+            _this.simulationStart();
+            $("#simulationStart").addClass("disabled");
+            $("#simulationStop").removeClass("disabled");
+        });
+
+
+        $("#simulationStop").on("click", function(){
+            _this.simulationStop();
+            $("#simulationStop").addClass("disabled");
+            $("#simulationStart").removeClass("disabled");
+        });
+
     },
 
     /**
@@ -389,11 +533,133 @@ var View = draw2d.Canvas.extend({
      **/
     onDrop : function(droppedDomNode, x, y, shiftKey, ctrlKey)
     {
+        var _this = this;
         var type = $(droppedDomNode).data("shape");
         var figure = eval("new "+type+"();"); // jshint ignore:line
         // create a command for the undo/redo support
         var command = new draw2d.command.CommandAdd(this, figure, x, y);
         this.getCommandStack().execute(command);
+
+        figure.on("contextmenu", function(){
+            var pathToFile   = "https://github.com/freegroup/draw2d_js.shapes/blob/master/"+ eval(figure.NAME+".github");
+            var pathToDesign = "http://freegroup.github.io/draw2d_js.app.shape_designer/?file="+ figure.NAME+".shape";
+            $.contextMenu({
+                selector: 'body',
+                events:
+                {
+                    hide:function(){ $.contextMenu( 'destroy' ); }
+                },
+                callback: $.proxy(function(key, options)
+                {
+                    switch(key){
+                        case "code":
+                            new CodeDialog().show( eval(figure.NAME+".logic"));
+                            break;
+                        case "design":
+                            window.open(pathToDesign);
+                            break;
+                        case "bug":
+                            var pathToIssues = "https://github.com/freegroup/draw2d_js.shapes/issues/new";
+                            var createUrl = pathToIssues+"?title=Error in shape '"+figure.NAME+"'&body="+encodeURIComponent("I found a bug in "+figure.NAME+".\n\nError Description here...\n\n\nLinks to the code;\n[GitHub link]("+pathToFile+")\n[Designer Link]("+pathToDesign+")\n");
+                            window.open(createUrl);
+                            break;
+                        case "delete":
+                            var cmd = new draw2d.command.CommandDelete(figure);
+                            _this.getCommandStack().execute(cmd);
+                            break;
+                        default:
+                            break;
+                    }
+
+                },this),
+                x:x,
+                y:y,
+                items:
+                {
+                    "code":    {name: "Show Code"},
+                    "design":  {name: "Open in Designer"},
+                    "bug":     {name: "Report a Bug"},
+                    "sep1":  "---------",
+                    "delete":{name: "Delete"}
+                }
+            });
+
+        });
+    },
+
+
+    simulationStart:function()
+    {
+        this.simulate=true;
+
+        this.installEditPolicy(new SimulationEditPolicy());
+        this.uninstallEditPolicy(this.connectionPolicy);
+        this.uninstallEditPolicy(this.coronaFeedback);
+        this.commonPorts.each(function(i,p){
+            p.setVisible(false);
+        });
+        requestAnimationFrame(this.animationFrameFunc);
+    },
+
+    simulationStop:function()
+    {
+        this.simulate = false;
+        this.commonPorts.each(function(i,p){
+            p.setVisible(true);
+        });
+        this.installEditPolicy(new EditEditPolicy());
+        this.installEditPolicy(this.connectionPolicy);
+        this.installEditPolicy(this.coronaFeedback);
+
+    },
+
+    _calculate:function()
+    {
+        // call the "calculate" method if given to calculate the output-port values
+        //
+        var figures = this.getFigures().clone().grep(function(f){
+            return f['calculate'];
+        });
+        figures.each(function(i,figure){
+            figure.calculate();
+        });
+
+        // transport the value from oututPort to inputPort
+        //
+        this.getLines().each(function(i,line){
+            var outPort = line.getSource();
+            var inPort  = line.getTarget();
+            inPort.setValue(outPort.getValue());
+            line.setColor(outPort.getValue()?"#ff5252":"#0000ff");
+        });
+
+        if(this.simulate===true){
+            requestAnimationFrame(this.animationFrameFunc);
+        }
+    },
+
+    /**
+     * @method
+     * Sent when an event occurs on the command stack. draw2d.command.CommandStackEvent.getDetail()
+     * can be used to identify the type of event which has occurred.
+     *
+     * @template
+     *
+     * @param {draw2d.command.CommandStackEvent} event
+     **/
+    stackChanged:function(event)
+    {
+        $("#editUndo").addClass("disabled");
+        $("#editRedo").addClass("disabled");
+
+        if(event.getStack().canUndo()) {
+            $("#editUndo").removeClass("disabled");
+        }
+
+        if(event.getStack().canRedo()) {
+            $("#editRedo").removeClass("disabled");
+        }
+
     }
 });
 
@@ -429,6 +695,20 @@ About = Class.extend(
 
       
 });  
+;
+var CodeDialog = Class.extend(
+    {
+
+        init:function(){
+        },
+
+        show:function(js){
+            $('#codePreviewDialog .prettyprint').text(js);
+            $('#codePreviewDialog .prettyprint').removeClass("prettyprinted");
+            prettyPrint();
+            $('#codePreviewDialog').modal('show');
+        }
+});
 ;
 FileOpen = Class.extend({
 
