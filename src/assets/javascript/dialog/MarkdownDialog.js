f00cfe936b88a242c6c23eac7dfa00ebefb13a1d
Herz, Andreas <andreas.herz@sap.com>
2016-09-21 18:19:37 +0200

    fixing doc and add HTML tag as markdown

--- src/assets/javascript/dialog/MarkdownDialog.js
+++ src/assets/javascript/dialog/MarkdownDialog.js
@@ -4,12 +4,12 @@ var MarkdownDialog = Class.extend(
         init:function()
         {
             this.defaults = {
-                html:         false,        // Enable HTML tags in source
+                html:         true,         // Enable HTML tags in source
                 xhtmlOut:     false,        // Use '/' to close single tags (<br />)
                 breaks:       false,        // Convert '\n' in paragraphs into <br>
                 langPrefix:   'language-',  // CSS language prefix for fenced blocks
                 linkify:      true,         // autoconvert URL-like texts to links
-                linkTarget:   '',           // set target to open link in
+                linkTarget:   '_blank',     // set target to open link in
                 typographer:  true          // Enable smartypants and other sweet transforms
             };
         },
